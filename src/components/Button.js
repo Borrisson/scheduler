f330e98cbae446fab5eb359d761b479455c53cf2
Luc <lucborris@gmail.com>
2021-02-15 13:00:03 -0500

    Add logic to class selection for button

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,19 @@ import React from "react";
 
 import "components/Button.scss";
 
+import classNames from "classnames";
+
 export default function Button(props) {
-   return <></>;
+  return (
+    <button
+      className={classNames("button", {
+        ["button--confirm"]: props.confirm,
+        ["button--danger"]: props.danger,
+      })}
+      disabled={props.disabled}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
+  );
 }
