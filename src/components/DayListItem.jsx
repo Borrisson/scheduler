699e902bca26c742eb0e127bc162885dd8356660
Luc <lucborris@gmail.com>
2021-02-15 13:36:15 -0500

    Add logic for class selection of DayListItem

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -4,12 +4,21 @@ import classNames from "classnames";
 
 import "./DayListItem.scss";
 
-
-export default function DayListItem({name, spots, selected, setDay, ...rest}) {
+export default function DayListItem({
+  name,
+  spots,
+  selected,
+  setDay,
+  ...rest
+}) {
+  const classStyle = classNames("day-list__item", {
+    ["day-list__item--selected"]: selected,
+    ["day-list__item--full"]: !spots,
+  });
   return (
-    <li onClick={() => setDay(name)}>
+    <li className={classStyle} onClick={() => setDay(name)}>
       <h2 className="text--regular">{name}</h2>
       <h3 className="text--light">{spots}</h3>
     </li>
-  )
+  );
 }
