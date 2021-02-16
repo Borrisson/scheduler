5c792212c781d203a1ba26d1e0316ee3740ac690
Luc <lucborris@gmail.com>
2021-02-16 12:49:47 -0500

    Fix eslint of unecessary bracket notation of button style names

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -8,8 +8,8 @@ export default function Button(props) {
   return (
     <button
       className={classNames("button", {
-        ["button--confirm"]: props.confirm,
-        ["button--danger"]: props.danger,
+        "button--confirm": props.confirm,
+        "button--danger": props.danger,
       })}
       disabled={props.disabled}
       onClick={props.onClick}
