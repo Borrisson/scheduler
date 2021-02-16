4295417fa84c018b478f35d5425e6771cacc9821
Luc <lucborris@gmail.com>
2021-02-16 07:50:47 -0500

    Add stories for appointment render and with a time

--- stories/index.js
+++ stories/index.js
@@ -136,3 +136,14 @@ storiesOf("InterviewerList", module)
       onChange={action("setInterviewer")}
     />
   ));
+
+// <---------- Appointment ----------->
+
+import Appointment from "components/Appointment/index";
+
+storiesOf("Appointment", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }],
+  })
+  .add("Appointment", () => <Appointment />)
+  .add("Appointment with Time", () => <Appointment time={"12pm"} />);
