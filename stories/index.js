0695e69e44642831c4e8c2ea5e098f891620f3a2
Luc <lucborris@gmail.com>
2021-02-16 12:48:47 -0500

    Add story for Form componenet of Appointment

--- stories/index.js
+++ stories/index.js
@@ -146,6 +146,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 storiesOf("Appointment", module)
   .addParameters({
@@ -164,4 +165,20 @@ storiesOf("Appointment", module)
   .add("Status", () => <Status message="Deleting" />)
   .add("Error", () => (
     <Error message="Could not delete appointment" onClose={action("onClose")} />
+  ))
+  .add("Form Create", () => (
+    <Form
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
+  ))
+  .add("Form Edit", () => (
+    <Form
+      name="Bryce McIssac"
+      interviewer={interviewers[0].id}
+      interviewers={interviewers}
+      onSave={action("onSave")}
+      onCancel={action("onCancel")}
+    />
   ));
