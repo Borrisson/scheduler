87e61b377e0a00c261c311a331e13a625be9284b
Luc <lucborris@gmail.com>
2021-02-16 13:55:42 -0500

    Add props to show component

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,7 +1,14 @@
-import React from "react";
-
+import React, { Fragment } from "react";
 import "./styles.scss";
+import Header from "components/Appointment/Header";
+import Show from "components/Appointment/Show";
+import Empty from "components/Appointment/Empty";
 
-export default function Appointment({ time, ...rest }) {
-  return <article className="appointment"></article>;
+export default function Appointment({ time, interview, ...rest }) {
+  return (
+    <article className="appointment">
+      <Header time={time} />
+      {interview ? <Show {...interview} /> : <Empty />}
+    </article>
+  );
 }
