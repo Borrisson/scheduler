f6bb76922540c38cabe2415951d226f1f3642952
Luc <lucborris@gmail.com>
2021-02-16 14:18:44 -0500

    Add Parsed Appointments array to be added to schedule section

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
-
 import "components/Application.scss";
-
 import DayList from "../components/DayList";
+import Appointment from "components/Appointment/index";
 
 const days = [
   {
@@ -22,9 +21,59 @@ const days = [
   },
 ];
 
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      },
+    },
+  },
+  {
+    id: 3,
+    time: "2pm",
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Bryce McIssac",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png",
+      },
+    },
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "Bryce McIssac",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png",
+      },
+    },
+  },
+];
+
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
 
+  const parsedAppointments = appointments.map((appointment) => {
+    return <Appointment key={appointment.id} {...appointment} />;
+  });
   return (
     <main className="layout">
       <section className="sidebar">
@@ -43,9 +92,7 @@ export default function Application(props) {
           alt="Lighthouse Labs"
         />
       </section>
-      <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
-      </section>
+      <section className="schedule">{parsedAppointments}</section>
     </main>
   );
 }
