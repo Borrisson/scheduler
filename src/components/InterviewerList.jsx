f0260f443b0776635e99a3361f14eafd2c488065
Luc <lucborris@gmail.com>
2021-02-15 20:33:32 -0500

    Add InterviewerList logic and style

---
+++ src/components/InterviewerList.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import InterviewerListItem from "./InterviewerListItem";
+
+import "./InterviewerList.scss";
+
+export default function InterviewerList({
+  interviewers,
+  interviewer,
+  setInterviewer,
+}) {
+  const parsedInterviewers = interviewers.map(({ id, name, avatar }) => (
+    <InterviewerListItem
+      id={id}
+      name={name}
+      avatar={avatar}
+      selected={id === interviewer}
+      setInterviewer={setInterviewer}
+    />
+  ));
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{parsedInterviewers}</ul>
+    </section>
+  );
+}
