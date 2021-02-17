d7a89ca0237a245586fe9d9c131012d82ab300c3
Luc <lucborris@gmail.com>
2021-02-17 14:31:55 -0500

    Change param name filterDay to make it more readable

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,5 +1,5 @@
-export function getAppointmentsForDay({ days, appointments }, day) {
-  const filteredDay = days.find((filterDay) => filterDay.name === day);
+export function getAppointmentsForDay({ days, appointments }, filterDay) {
+  const filteredDay = days.find((day) => day.name === filterDay);
 
   return filteredDay
     ? filteredDay.appointments.map((id) => appointments[id])
