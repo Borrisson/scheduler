fa6c03b279f645da658760048f0561f02fad0177
Luc <lucborris@gmail.com>
2021-02-18 09:16:00 -0500

    Add getInterviewersForDay function to pass interviewers prop the proper array

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -3,7 +3,11 @@ import "components/Application.scss";
 import DayList from "../components/DayList";
 import Appointment from "components/Appointment/index";
 import axios from "axios";
-import { getAppointmentsForDay, getInterview } from "../helpers/selectors";
+import {
+  getAppointmentsForDay,
+  getInterview,
+  getInterviewersForDay,
+} from "../helpers/selectors";
 
 export default function Application(props) {
   const [state, setState] = useState({
@@ -29,6 +33,7 @@ export default function Application(props) {
   }, []);
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
+  const dailyInterviewers = getInterviewersForDay(state, state.day);
 
   const parsedAppointments = dailyAppointments.map((appointment) => {
     const interview = getInterview(state, appointment.interview);
@@ -38,6 +43,7 @@ export default function Application(props) {
         key={appointment.id}
         {...appointment}
         interview={interview}
+        interviewers={dailyInterviewers}
       />
     );
   });
