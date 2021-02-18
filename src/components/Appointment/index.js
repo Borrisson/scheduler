6dace327289587836c18b38fbf46ba1d3f33dfe4
Luc <lucborris@gmail.com>
2021-02-18 07:36:12 -0500

    Add mode manager for Appointment states inside Appointment Component (Only EMPTY and SHOW ATM

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,12 +3,22 @@ import "./styles.scss";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import useVisualMode from "hooks/useVisualMode";
+import Form from "./Form";
 
 export default function Appointment({ time, interview, ...rest }) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY);
+
   return (
     <article className="appointment">
       <Header time={time} />
-      {interview ? <Show {...interview} /> : <Empty />}
+      {mode === "EMPTY" && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && <Show {...interview} />}
+      {mode === CREATE && <Form interviewers={[]} />}
     </article>
   );
 }
