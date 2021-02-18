ed14e67532ca6bc83a70b6298c5fdd4d3c24b0d4
Luc <lucborris@gmail.com>
2021-02-18 14:54:39 -0500

    Add id prop to onCancel on the onClick Event

--- src/components/Appointment/Confirm.jsx
+++ src/components/Appointment/Confirm.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import Button from "components/Button";
 
-export default function Confirm({ message, onConfirm, onCancel, ...rest }) {
+export default function Confirm({ id, message, onConfirm, onCancel, ...rest }) {
   return (
     <main className="appointment__card appointment__card--confirm">
       <h1 className="text--semi-bold">Delete the appointment?</h1>
@@ -10,7 +10,7 @@ export default function Confirm({ message, onConfirm, onCancel, ...rest }) {
         <Button onClick={onCancel} danger>
           Cancel
         </Button>
-        <Button onClick={onConfirm} danger>
+        <Button onClick={() => onConfirm(id)} danger>
           Confirm
         </Button>
       </section>
