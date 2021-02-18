6fb5fe4480487b9c80255590397fba92f19253aa
Luc <lucborris@gmail.com>
2021-02-18 13:59:23 -0500

    Add axios put request for appointments

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -18,7 +18,26 @@ export default function Application(props) {
   const setDay = (day) => setState({ ...state, day });
 
   function bookInterview(id, interview) {
-    console.log(id, interview);
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview },
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment,
+    };
+
+    setState({
+      ...state,
+      appointments,
+    });
+
+    return axios
+      .put(`/api/appointments/${id}`, appointment)
+      .then(() => {
+        setState(...state);
+      })
+      .catch((err) => console.log(err.message));
   }
 
   useEffect(() => {
