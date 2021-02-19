6c942c9a7a9dcc7b2cd0366e8ee3785f4cd44346
Luc <lucborris@gmail.com>
2021-02-19 09:17:39 -0500

    Add setSpots logic

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { getAppointmentsForDay } from "../helpers/selectors";
 export default function useApplicationData() {
   const [state, setState] = useState({
     day: "Monday",
@@ -8,6 +9,24 @@ export default function useApplicationData() {
   });
 
   const setDay = (day) => setState({ ...state, day });
+
+  function setSpots(deleting = false) {
+    const toggle = deleting ? -1 : 1;
+    const appointmentsForDay = getAppointmentsForDay(state, state.day);
+    const spots = appointmentsForDay.length;
+    const spotsRemaining =
+      spots -
+      appointmentsForDay
+        .map(({ interview }) => (interview ? 1 : 0))
+        .reduce((prev, curr) => prev + curr, toggle);
+
+    const currDay = state.days.findIndex(({ name }) => name === state.day);
+    const days = state.days.map((obj) => {
+      return { ...obj };
+    });
+    days.find((el, index) => index === currDay).spots = spotsRemaining;
+    return days;
+  }
 
   function bookInterview(id, interview) {
     const appointment = {
@@ -19,15 +38,24 @@ export default function useApplicationData() {
       [id]: appointment,
     };
 
+    const days = setSpots();
+
     setState({
       ...state,
       appointments,
+      days,
     });
 
     return axios.put(`/api/appointments/${id}`, appointment);
   }
 
   function cancelInterview(id) {
+    const days = setSpots(true);
+
+    setState({
+      ...state,
+      days,
+    });
     return axios.delete(`/api/appointments/${id}`);
   }
 
