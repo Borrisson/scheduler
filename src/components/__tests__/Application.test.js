5b587518b23f59c9d358d08d4a65079bb92079a1
Luc <lucborris@gmail.com>
2021-02-23 08:18:50 -0500

    Add defaults to Monday... test to Application test suite

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,13 +1,15 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, waitForElement } from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
 describe("Application", () => {
-  it("renders without crashing", () => {
-    render(<Application />);
+  it("defaults to Monday and changes schedule when new day is selected", () => {
+    const { getByText } = render(<Application />);
+
+    return waitForElement(() => getByText("Monday"));
   });
 });
