64065701e74d5b6a2589bc03559a588702819ece
Luc <lucborris@gmail.com>
2021-02-23 15:48:06 -0500

    Add delete appointment tests

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -12,9 +12,11 @@ import {
   getByPlaceholderText,
   getAllByTestId,
   queryByText,
+  queryByAltText,
 } from "@testing-library/react";
 
 import Application from "components/Application";
+import { configureActions } from "@storybook/addon-actions/dist/preview";
 
 afterEach(cleanup);
 
@@ -28,7 +30,7 @@ describe("Application", () => {
     expect(queryByText("Archie Cohen")).not.toBeInTheDocument();
   });
   it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
-    const { container, debug } = render(<Application />);
+    const { container } = render(<Application />);
     await waitForElement(() => getByText(container, "Archie Cohen"));
     const firstAdd = getAllByAltText(container, "Add")[0];
     fireEvent.click(firstAdd);
@@ -44,4 +46,26 @@ describe("Application", () => {
     );
     expect(getByText(day, "no spots remaining")).toBeInTheDocument();
   });
+  it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+    const { container } = render(<Application />);
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+    const appointment = getAllByTestId(
+      container,
+      "appointment"
+    ).find((appointment) => queryByText(appointment, "Archie Cohen"));
+    fireEvent.click(queryByAltText(appointment, "Delete"));
+    expect(
+      getByText(appointment, "Delete the appointment?")
+    ).toBeInTheDocument();
+    fireEvent.click(queryByText(appointment, "Confirm"));
+    expect(getByText(appointment, "Deleting")).toBeInTheDocument();
+    await waitForElement(() => getByAltText(appointment, "Add"));
+    const day = getAllByTestId(container, "day").find((day) =>
+      queryByText(day, "Monday")
+    );
+    expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
+  });
+  xit("loads data, edits an interview and keeps the spots remaining for Monday the same", () => {});
+  xit("shows the save error when failing to save an appointment", () => {});
+  xit("shows the delete error when failing to delete an existing appointment", () => {});
 });
